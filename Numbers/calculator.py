d46ae77d24c2f87d1ea5a8193e395d85a3930405
ChristopherFont <64507939+ChristopherFont@users.noreply.github.com>
2020-06-15 15:52:39 -0400

    Add files via upload

---
+++ Numbers/calculator.py
@@ -0,0 +1,39 @@
+# A basic calculator function
+
+def main():
+    number1 = 0
+    number2 = 0
+    op = " "
+
+    while True:
+        try:
+            number1 = int(input("Enter a number: "))
+            break;
+        except ValueError:
+            print("Error! Invalid input. Try again.")
+
+    while True:
+        try:
+            number2 = int(input("Enter another number: "))
+            break;
+        except ValueError:
+            print("Error! Invalid input. Try again.")
+
+    while op not in ('-','+','*','/'):
+        op = str(input("Enter a valid operator (+-*/): "))
+        print("Error! Invalid input. Try again.")
+
+    print(number1, op, number2, '=', calculate(number1, number2, op))
+
+def calculate(n,m,op):
+    tmp = 0
+    if op == '+':
+        return n + m
+    elif op == '-':
+        return n - m
+    elif op == '*':
+        return n * m
+    return n / m
+
+if __name__ == "__main__":
+    main()
